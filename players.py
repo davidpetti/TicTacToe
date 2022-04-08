b504005a8a99ee1da04ec8c872c11e80a651bdc5
davidpetti <81206758+davidpetti@users.noreply.github.com>
2022-04-08 14:07:06 +0000

    fixed tab spacing

--- players.py
+++ players.py
@@ -1,23 +1,23 @@
 class Player:
-  def __init__(self, letter):
-    self.letter = letter
+    def __init__(self, letter):
+        self.letter = letter
+
 
 class HumanPlayer(Player):
-  def __init__(self, letter):
-    super().__init__(letter)
+    def __init__(self, letter):
+        super().__init__(letter)
 
-  def get_move(self, game):
-    valid = False
-    while valid == False:
-      game.print_numboard()
-      print()
-      square = input(f"{self.letter}'s turn\nEnter a square: ")
-      try:
-        if int(square) in game.available_moves:
-          valid = True
-          return square
-        else:
-          print("Move not available")
-      except ValueError:
-        print("Invalid Input - Try again\n")
-        
+    def get_move(self, game):
+        valid = False
+        while valid == False:
+            game.print_numboard()
+            print()
+            square = input(f"{self.letter}'s turn\nEnter a square: ")
+            try:
+                if int(square) in game.available_moves:
+                    valid = True
+                    return square
+                else:
+                    print("Move not available")
+            except ValueError:
+                print("Invalid Input - Try again\n")
